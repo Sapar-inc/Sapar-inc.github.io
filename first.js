4a69f9e60993031ee0ec76c9b44ccdc3441499e3
Sapar Saparov <you@example.com>
2022-11-08 13:38:34 +0600

    update to do list

--- first.js
+++ first.js
@@ -1,5 +1,9 @@
 let form = document.querySelector("#form")
 let input = document.querySelector("#text")
+let clearBtn = document.querySelector("#clear_list")
+let inputRemove = document.querySelector("#text_remove")
+let removeBtn = document.querySelector("#remove_list")
+
 
 form.addEventListener("submit", (e)=>{
     e.preventDefault() // .preventDefaulf отменяет отправку данных на сервер!!!
@@ -8,3 +12,16 @@ form.addEventListener("submit", (e)=>{
     localStorage.setItem("list",JSON.stringify(list))
     input.value = ""
 })
+
+removeBtn.addEventListener("click", () =>{
+    let list = JSON.parse(localStorage.getItem("list")) || []
+    list.splice(inputRemove.value, 1)
+    localStorage.setItem("list",JSON.stringify(list))
+})
+
+clearBtn.addEventListener("click", ()=>{
+    let list = JSON.parse(localStorage.getItem("list")) || []
+    localStorage.clear()
+    console.log(localStorage.getItem("list",list))
+})
+
